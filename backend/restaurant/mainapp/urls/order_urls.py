581793bc634f2e9d0ed2adedf551c760240c05b4
Jacek Pilch <jaaahooou@wp.pl>
2023-02-06 20:05:56 +0100

    update

--- backend/restaurant/mainapp/urls/order_urls.py
+++ backend/restaurant/mainapp/urls/order_urls.py
@@ -5,5 +5,5 @@ from mainapp.views import order_views as views
 urlpatterns = [
     path('get-orders',views.getOrders, name="get-orders" ),
     path('add-dish-to-order',views.addDishToOrder, name="add-dish-to-order" ),
-    
+    #g
 ]
