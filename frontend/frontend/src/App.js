aa99dde10760a093c75fa2fdec6eafe0ca5a0b12
Jacek Pilch <jaaahooou@wp.pl>
2023-02-07 08:51:01 +0100

    prepare react files

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+function App() {
+  return (
+    <div className="App">
+      <div>MY app</div>
+    </div>
+  );
+}
+
+export default App;
