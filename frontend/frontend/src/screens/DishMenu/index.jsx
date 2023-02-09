8073b2bc59db00065d4fcf046ffbafd333d85e40
Jacek Pilch <jaaahooou@wp.pl>
2023-02-09 07:56:36 +0100

    restaurant menu elements

--- frontend/frontend/src/screens/DishMenu/index.jsx
+++ frontend/frontend/src/screens/DishMenu/index.jsx
@@ -22,17 +22,27 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Typography } from "@mui/material";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
+const categories = [
+  "Salads",
+  "Aperitif",
+  "Soups",
+  "Main dishes",
+  "Beers",
+  "Soft drinks",
+];
+
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159),
+  createData("Ice cream sandwich", 250),
+  createData("Eclair", 262),
+  createData("Cupcake", 305),
+  createData("Gingerbread", 250),
 ];
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -54,15 +64,16 @@ export default function ResponsiveGrid() {
         {Array.from(Array(6)).map((_, index) => (
           <Grid item xs={1} sm={4} md={4} key={index}>
             <Item>
+              <Typography variant="h4" align="left">
+                {categories[index]}
+              </Typography>
+
               <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <Table sx={{ maxWidth: "100%" }} aria-label="simple table">
                   <TableHead>
                     <TableRow>
-                      <TableCell>Dessert (100g serving)</TableCell>
-                      <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                      <TableCell>Name</TableCell>
+                      <TableCell align="right">Price</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
@@ -77,9 +88,6 @@ export default function ResponsiveGrid() {
                           {row.name}
                         </TableCell>
                         <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
